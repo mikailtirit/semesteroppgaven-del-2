0342050ad532ca2e050f2a16f2eb0735719cd6da
mikailtirit <mikail.tirit@hotmail.com>
2026-04-27 09:28:52 +0000

    Create RegisterForm with username, password, and submit fields

--- forms.py
+++ forms.py
@@ -2,7 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired
 
+
 class registerForm(FlaskForm):
     username = StringField("brukernavn", validators=[InputRequired()])
     Password = PasswordField("passord", validators=[InputRequired()])
-    submit = SubmitField("register")        
+    submit = SubmitField("register")      
