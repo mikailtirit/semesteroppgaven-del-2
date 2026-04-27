830f4d7ca1bbf82f64e3ace09b661e5c0612d453
mikailtirit <mikail.tirit@hotmail.com>
2026-04-27 12:47:35 +0000

    Forbedret register-route med POST-håndtering og passord-hashing
    Oppdatert register-ruten til å støtte både GET og POST requests. Lagt til logikk for å hente data fra RegisterForm og implementert passord-hashing med generate_password_hash for bedre sikkerhet.
    
    Dette gjør at applikasjonen nå kan ta imot brukerinput, behandle skjema-data og sikre passord før videre bruk. Returnerer foreløpig en testrespons for å verifisere at innsending av skjema og hashing fungerer korrekt.

--- app.py
+++ app.py
@@ -9,7 +9,17 @@ app.config['SECRET_KEY'] = 'devkey'
 def home():
     return render_template('home.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+
+    if request.method == 'POST':
+        username = form.username.data
+        password = form.password.data
+
+    
+        hashed_password = generate_password_hash(password)
+
+        return f"Username: {username} | Hashed Password: {hashed_password}"
+
     return render_template('register.html', form=form)
